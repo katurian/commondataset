8867a43b2a7bc39638dfd6cfdc497b0ef2132313
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-05-09 00:47:04 -0400

    Create pareto.r

---
+++ UNC/pareto.r
@@ -0,0 +1,10 @@
+ggplot(CDSGrouped, aes(x = Year, y = Tuition, fill = TuitionType)) +
+  geom_col(position = "dodge") +
+  geom_line() +
+  ggtitle("Tuition at UNC Chapel Hill (2010-2020)", subtitle = "Source: https://oira.unc.edu/reports/reports-archives/common-data-set/") +
+  xlab("") +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  ylab("") +
+  labs(fill = "") +
+  scale_colour_economist() +
+  theme_economist_white(base_size = 11, base_family= "Veranda", gray_bg = TRUE, horizontal = TRUE) 
